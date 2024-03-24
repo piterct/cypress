95d736994a814aba8ade62e2fef731d6a8bcc5dc
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-24 14:16:44 -0300

    elements

---
+++ cypress/integration/elements.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Work with basic elements', () => {
+    it('Text', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('span').should('contain','Cuidado')
+        cy.get('.facilAchar').should('contain','Cuidado')
+        cy.get('.facilAchar').should('have.text','Cuidado onde clica, muitas armadilhas...')
+    }) 
+})
