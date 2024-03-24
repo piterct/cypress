bb1ee7027ac4728bbfbf5da8d41726797edb4d41
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-24 00:55:29 -0300

    visit a website

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('Crypress basics', () => {
+    it('Should visit a page and assert title', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+       // const title = cy.title()
+       // console.log(title)
+
+       cy.title().should('be.equal', 'Campo de Treinamento')
+       cy.title().should('contain', 'Campo')
+
+       cy.title()
+                 .should('be.equal', 'Campo de Treinamento')
+                 .and('contain', 'Campo')
+
+         //TODO Print the title inside the console
+         //TODO Write the title in a text field       
+    })
+})
