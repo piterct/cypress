e772823b6a8f5b8e2c7390d8233d7dda9fc86e2b
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-24 01:14:07 -0300

    validate element after click the button

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -17,4 +17,13 @@ describe('Crypress basics', () => {
          //TODO Print the title inside the console
          //TODO Write the title in a text field       
     })
+
+    it.only('Should find and interect with an element', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        //cy.get('not exist')
+        cy.get('#buttonSimple')
+             .click()
+             .should('have.value', 'Obrigado!')
+    })
 })
