b072b57e38165c591a217731be10441dbcedcccf
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-24 14:31:28 -0300

    elements with links

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -8,3 +8,14 @@ describe('Work with basic elements', () => {
         cy.get('.facilAchar').should('have.text','Cuidado onde clica, muitas armadilhas...')
     }) 
 })
+
+it.only('Links', () => {
+    cy.visit('https://wcaquino.me/cypress/componentes.html')
+    cy.get('[href="#"]').click()
+    cy.get('#resultado').should('have.text','Voltou!')
+
+    cy.reload()
+    cy.get('#resultado').should('have.not.text','Voltou!')
+    cy.contains('Voltar').click()
+    cy.get('#resultado').should('have.text','Voltou!')
+})
