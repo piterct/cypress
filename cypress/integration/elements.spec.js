65d56c9cdb287e0e76cdca9944e7d4be92b5781b
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-26 05:28:20 -0300

    CheckBox

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -11,55 +11,67 @@ describe('Work with basic elements', () => {
     })
 
     it('Text', () => {
-        cy.get('span').should('contain','Cuidado')
-        cy.get('.facilAchar').should('contain','Cuidado')
-        cy.get('.facilAchar').should('have.text','Cuidado onde clica, muitas armadilhas...')
+        cy.get('span').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
 
-   it('Links', () => {
-    cy.get('[href="#"]').click()
-    cy.get('#resultado').should('have.text','Voltou!')
+    it('Links', () => {
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
 
-    cy.reload()
-    cy.get('#resultado').should('have.not.text','Voltou!')
-    cy.contains('Voltar').click()
-    cy.get('#resultado').should('have.text','Voltou!')
-   })
+        cy.reload()
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+    })
 
-   it("Text Fields", () => {
-    cy.get('#formNome').type('Cypress Test')
-    cy.get('#formNome').should('have.value', 'Cypress Test')
+    it("Text Fields", () => {
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value', 'Cypress Test')
 
-    cy.get('#elementosForm\\:sugestoes')
-      .type('textArea')
-      .should('have.value', 'textArea')
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textArea')
+            .should('have.value', 'textArea')
 
-    cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
-      .type("???")
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type("???")
 
-      cy.get('[data-cy="dataSobrenome"]')
-      .type('Test12345{backspace}{backspace}')
-      .should('have.value', 'Test123')
+        cy.get('[data-cy="dataSobrenome"]')
+            .type('Test12345{backspace}{backspace}')
+            .should('have.value', 'Test123')
 
-      cy.get('#elementosForm\\:sugestoes')
-      .clear()
-      .type('Error{selectall}adjusted', {delay: 100})
-      .should('have.value', 'adjusted')
-   })
-   
-   it('Radio Button', () => {
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Error{selectall}adjusted', { delay: 100 })
+            .should('have.value', 'adjusted')
+    })
 
-    cy.get('#formSexoFem')
-       .click()
-       .should('be.checked')
+    it('Radio Button', () => {
 
-    cy.get('#formSexoMasc')
-       .should('not.be.checked')
+        cy.get('#formSexoFem')
+            .click()
+            .should('be.checked')
 
-    cy.get("[name=formSexo]")
-       .should('have.length', 2)
+        cy.get('#formSexoMasc')
+            .should('not.be.checked')
 
-   })
+        cy.get("[name=formSexo]")
+            .should('have.length', 2)
+
+    })
+
+    it('CheckBox', () => {
+        cy.get('#formComidaPizza')
+            .click()
+            .should('be.checked')
+
+        cy.get('[name=formComidaFavorita]').click({ multiple: true })
+        cy.get('#formComidaPizza').should('not.be.checked')
+        cy.get('#formComidaVegetariana').should('be.checked')
+
+    })
 
 })
+
 
