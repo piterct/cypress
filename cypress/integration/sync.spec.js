9e645a91a735c53098879098e5dd5d54f19086cb
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-28 07:13:13 -0300

    sync

---
+++ cypress/integration/sync.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+
+describe('Waits...', () => {
+    beforeEach(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+
+    it('Must wait available element', () => {
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo').should('exist')
+        cy.get('#novoCampo').type('funciona')
+    })
+
+
+})
