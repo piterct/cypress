e707372671628d4cab001ccbf0981bf69f204c04
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-28 07:01:57 -0300

    Multiple Combobox

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -69,18 +69,27 @@ describe('Work with basic elements', () => {
         cy.get('[name=formComidaFavorita]').click({ multiple: true })
         cy.get('#formComidaPizza').should('not.be.checked')
         cy.get('#formComidaVegetariana').should('be.checked')
-
     })
 
-    it.only('ComboBox', () => {
+    it('ComboBox', () => {
         cy.get('[data-test="dataEscolaridade"]')
             .select('2o grau completo')
             .should('have.value', '2graucomp')
 
-            cy.get('[data-test="dataEscolaridade"]')
+        cy.get('[data-test="dataEscolaridade"]')
             .select('1graucomp')
             .should('have.value', '1graucomp')
+
+        //TODO Validate ComboBox options
     })
+
+    it.only('Multiple ComboBox', () => {
+        cy.get('[data-testid="dataEsportes"]')
+            .select(['natacao', 'Corrida', 'nada'])
+
+        //TODO  Validate selected Multiple ComboBox options
+    })
+
 
 })
 
