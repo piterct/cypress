d07306040ac868c560f98222f272b03deddb442c
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-31 21:58:49 -0300

    promisses using should

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,33 +1,39 @@
 /// <reference types="cypress" />
 
 describe('Crypress basics', () => {
-    it('Should visit a page and assert title', () => {
+    it.only('Should visit a page and assert title', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
-       // const title = cy.title()
-       // console.log(title)
+        // const title = cy.title()
+        // console.log(title)
 
-       cy.pause()
+        //cy.pause()
 
-       cy.title().should('be.equal', 'Campo de Treinamento')
-       cy.title().should('contain', 'Campo')
+        cy.title().should('be.equal', 'Campo de Treinamento')
+        cy.title().should('contain', 'Campo')
 
-       cy.title()
-                 .should('be.equal', 'Campo de Treinamento')
-                 .and('contain', 'Campo')
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
 
-         //TODO Print the title inside the console
-         //TODO Write the title in a text field       
+        cy.title()
+
+        cy.title().should(title => {
+            console.log(title)
+        })
+
+        //TODO Print the title inside the console
+        //TODO Write the title in a text field       
     })
 
-    it.only('Should find and interect with an element', () => {
+    it('Should find and interect with an element', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
         cy.pause()
 
         //cy.get('not exist')
         cy.get('#buttonSimple')
-             .click()
-             .should('have.value', 'Obrigado!')
+            .click()
+            .should('have.value', 'Obrigado!')
     })
 })
