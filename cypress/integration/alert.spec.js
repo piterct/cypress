c7846817b9c0536b339066748a537a61a1b67af3
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-04-13 23:50:47 -0300

    Validate Confirm Alert

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -18,11 +18,18 @@ describe('Work with basic alerts', () => {
         })
     })
 
-    it.only('Alert with mock', () => {
+    it('Alert with mock', () => {
         const stub = cy.stub().as('alert')
         cy.on('window:alert', stub)
         cy.get('#alert').click().then(() => {
             expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
+        })
+    })
+
+    it.only('Confirm', () => {
+        cy.get('#confirm').click()
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
         })
     })
 })
