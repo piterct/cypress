457ef076322e3b8c22c97090626cbd905e001218
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-04-13 23:23:57 -0300

    Alert File

---
+++ cypress/integration/alert.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+
+describe('Work with basic elements', () => {
+    beforeEach(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+})
