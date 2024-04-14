6ee78f37c88312d0f057737464f25301d2779afd
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-04-14 19:42:17 -0300

    locators test

---
+++ cypress/integration/locators.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Work with basic elements', () => {
+    beforeEach(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Locators', () => {
+
+    })
+})
