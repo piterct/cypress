e0688b7199b45b528fe9020ceba71d11fe2864df
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-04-14 20:38:23 -0300

    Using jquery selector

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -9,7 +9,12 @@ describe('Work with basic elements', () => {
         cy.reload()
     })
 
-    it('Locators', () => {
+    it('Using jquery selector', () => {
+        cy.get(':nth-child(1) > :nth-child(3) > [type="button"]')
+        cy.get('table#tabelaUsuarios tbody > tr:eq(0) td:nth-child(3) > input')
+        cy.get("[onclick*='Francisco']")
 
+        cy.get('table#tabelaUsuarios td:contains(\'Doutorado\'):eq(0) ~ td:eq(3) input ')
+        cy.get('table#tabelaUsuarios tr:contains(\'Doutorado\'):eq(0) td:eq(6) input')
     })
 })
