bd1f4b85e622f2e2a7a3c8484f29093a84adabc8
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-04-19 22:31:06 -0300

    locations

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -19,6 +19,8 @@ describe('Work with basic elements', () => {
     })
 
     it('Using xpath', () => {
-        cy.xpath('//input')
+        cy.xpath('//input[contains(@onclick,\'Francisco\')]')
+        cy.xpath("//table[@id=\'tabelaUsuarios\']//td[contains(.,'Francisco')]/..//input[@type='text']")
+        cy.xpath("//td[contains(.,'Usuario A')]/following-sibling::td[contains(., 'Mestrado')]//..//input[@type='text']").type('works')
     })
 })
