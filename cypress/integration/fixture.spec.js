397b3e22a134f1b266e8e2c51e35905de4cba68a
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-04-22 21:40:26 -0300

    user registered

--- cypress/integration/fixture.spec.js
+++ cypress/integration/fixture.spec.js
@@ -1,16 +1,18 @@
 /// <reference types="cypress" />
 
 describe('Fixture tests', () => {
-    it('Get data from fixture file', () => {
+    it('Get data from fixture file', function () {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
-        cy.get('#formNome').type()
-        cy.get('#formSobrenome').type()
-        cy.get('[name=formSexo][value=F]').click()
-        cy.get('[name=formComidaFavorita][value=pizza]').click()
-        cy.get('#formEscolaridade').select()
-        cy.get('#formEsportes').select()
-        cy.get('#formCadastrar')
+        cy.fixture('userData').as('user').then((() => {
+            cy.get('#formNome').type(this.user.name)
+            cy.get('#formSobrenome').type(this.user.lastName)
+            cy.get(`[name=formSexo][value=${this.user.gender}]`).click()
+            cy.get(`[name=formComidaFavorita][value=${this.user.food}]`).click()
+            cy.get('#formEscolaridade').select(this.user.education)
+            cy.get('#formEsportes').select(this.user.sport)
+        }))
 
+        cy.get('#formCadastrar').click()
         cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado')
     })
 })
