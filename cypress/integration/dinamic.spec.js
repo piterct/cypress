2f12e269f75692c581a9272d6ea2dc1c51d36896
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-04-22 22:12:58 -0300

    for each

---
+++ cypress/integration/dinamic.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('Dinamic tests', () => {
+    beforeEach(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    const foods = ['Carne', 'Frango', 'Pizza', 'Vegetariano']
+    foods.forEach(food => {
+        it('Register with variable food', () => {
+            cy.get('#formNome').type('User')
+            cy.get('#formSobrenome').type('Any')
+            cy.get(`[name=formSexo][value=F]`).click()
+            cy.xpath(`//label[contains(., '${food}')]/preceding-sibling::input`).click()
+            cy.get('#formEscolaridade').select('Doutorado')
+            cy.get('#formEsportes').select('Corrida')
+
+            cy.get('#formCadastrar').click()
+            cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado')
+        })
+    })
+
+
+})
