7fcf6ac9a3662b4e620b83a507544635ca2415fe
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-04-27 01:25:40 -0300

    login user locator

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,12 +1,14 @@
 /// <reference types="cypress" />
 
+import loc from '../../support/locators'
+
 describe('Should test at a functional level', () => {
     beforeEach(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
-        cy.get('.input-group > .form-control').type('piterct.teste@gmail.com')
-        cy.get(':nth-child(2) > .form-control').type('123')
-        cy.get('.btn').click()
-        cy.get('.toast-message').should('contain', 'Bem vindo')
+        cy.get(loc.LOGIN.USER).type('piterct.teste@gmail.com')
+        cy.get(loc.LOGIN.PASSWORD).type('123')
+        cy.get(loc.LOGIN.BTN_LOGIN).click()
+        cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
     })
 
     it('Should create an account', () => {
