63a655c96e985262d4cdc87fa14b6d7a499f54ab
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-04-27 20:29:55 -0300

    insert account and account menu commands

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,19 +1,17 @@
 /// <reference types="cypress" />
 
 import loc from '../../support/locators'
+import '../../support/commandAccounts'
 
 describe('Should test at a functional level', () => {
     before(() => {
         cy.login('piterct.teste@gmail.com', '123')
         cy.resetApp()
-        
     });
 
     it('Should create an account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.ACCOUNTS).click()
-        cy.get(loc.ACCOUNTS.NAME).type('Test account')
-        cy.get(loc.ACCOUNTS.BTN_SAVE).click()
+        cy.accessAccountMenu()
+        cy.insertAccount('Test account')
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso!')
     })
 
