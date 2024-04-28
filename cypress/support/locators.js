758940da95cb1da76ab164fe40ebaf6c06adce4a
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-04-27 21:19:03 -0300

    movimentation locators

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -7,12 +7,19 @@ const locators = {
     MENU: {
         SETTINGS: '[data-test=menu-settings',
         ACCOUNTS: '[href="/contas"]',
-        RESET: '[href="/reset"]'
+        RESET: '[href="/reset"]',
+        MOVIMENTATION: '[data-test=menu-movimentacao]'
     },
     ACCOUNTS: {
         NAME: '[data-test=nome]',
         BTN_SAVE: '.btn',
         XP_BTN_UPDATE: "//table//td[contains(.,'Test account')]/..//i[@class='far fa-edit']"
+    },
+    MOVIMENTATION: {
+        DESCRIPTION: '[data-test=descricao]',
+        VALUE: '[data-test=valor]',
+        INTERESTED: '[data-test="envolvido]',
+        BTN_SALVE: '.btn-primary'
     },
     MESSAGE: '.toast-message'
 
