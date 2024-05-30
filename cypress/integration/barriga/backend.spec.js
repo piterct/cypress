05f2d6bdae31ec0cb930cd4e43cbae0893246a7b
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-05-30 00:13:35 -0300

    reuse command gettoken to authorize other request

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Should test at a functional level', () => {
+    
     beforeEach(() => {
         //cy.login('piterct.teste@gmail.com', '123')
     });
@@ -10,15 +11,7 @@ describe('Should test at a functional level', () => {
     })
 
     it('Should create an account', () => {
-        cy.request({
-            method: 'POST',
-            url: 'https://barrigarest.wcaquino.me/signin',
-            body: {
-                email: "piterct.teste@gmail.com",
-                redirecionar: false,
-                senha: "123"
-            }
-        }).its('body.token').should('not.be.empty')
+        cy.getToken('piterct.teste@gmail.com', '123')
             .then(token => {
                 cy.request({
                     method: 'POST',
@@ -27,12 +20,12 @@ describe('Should test at a functional level', () => {
                     body: { nome: "conta qualquer4545" }
                 }).as('response')
             })
-     
-            cy.get('@response').then(res => {
-                expect(res.status).to.be.equal(201)
-                expect(res.body).to.be.property('id')
-                expect(res.body).to.have.property('nome', 'conta qualquer4545')
-            })
+
+        cy.get('@response').then(res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.be.property('id')
+            expect(res.body).to.have.property('nome', 'conta qualquer4545')
+        })
 
     })
 
