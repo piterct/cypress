5bdbc163ec9a1635f912ed1719a3d9078b5f176b
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-06-20 00:03:45 -0300

    after everthing must reset the values

--- cypress/integration/barriga/frontend.spec.js
+++ cypress/integration/barriga/frontend.spec.js
@@ -1,167 +1,71 @@
 /// <reference types="cypress" />
+import loc from '../../support/locators'
+import '../../support/commandAccounts'
 
 describe('Should test at a functional level', () => {
-
     beforeEach(() => {
-        cy.getToken('piterct.teste@gmail.com', '123')
+        cy.login('piterct.teste@gmail.com', '123')
     });
 
     afterEach(() => {
+        cy.resetApp()
+    })
+    after(() => {
         cy.resetRest()
     })
 
     it('Should create an account', () => {
-        cy.request({
-            method: 'POST',
-            url: '/contas',
-            body: { nome: "any account" }
-        }).as('response')
-
-        cy.get('@response').then(res => {
-            expect(res.status).to.be.equal(201)
-            expect(res.body).to.be.property('id')
-            expect(res.body).to.have.property('nome', 'any account')
-        })
+        cy.accessAccountMenu()
+        cy.insertAccount('Test account')
+        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso!')
     })
 
     it('Should update an account', () => {
-        cy.getAccountByName('Conta para alterar')
-            .then(accountId => {
-                cy.request({
-                    method: 'PUT',
-                    url: `/contas/${accountId}`,
-                    body: { nome: "Updated account by rest" }
-                }).as('response')
-
-                cy.get('@response').its('status').should('be.equal', 200)
-            })
+        cy.accessAccountMenu()
+        cy.xpath(loc.ACCOUNTS.FN_XP_BTN_UPDATE('Conta para alterar')).click()
+        cy.insertAccount('Update account')
+        cy.get(loc.ACCOUNTS.BTN_SAVE).click()
+        cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso!')
     })
 
     it('Should not create an account with same name', () => {
-        cy.request({
-            method: 'POST',
-            url: '/contas',
-            body: { nome: "Conta mesmo nome" },
-            failOnStatusCode: false
-        }).as('response')
-
-        cy.get('@response').then(res => {
-            expect(res.status).to.be.equal(400)
-            expect(res.body.error).to.be.equal('Já existe uma conta com esse nome!')
-        })
-
+        cy.accessAccountMenu()
+        cy.insertAccount('Conta mesmo nome')
+        cy.get(loc.MESSAGE).should('contain', 'Request failed with status code 400')
     })
 
     it('Should create a transaction', () => {
-        cy.getAccountByName('Conta para movimentacoes')
-            .then(accountId => {
-                const now = new Date();
-                cy.request({
-                    method: 'POST',
-                    url: '/transacoes',
-                    body: {
-                        conta_id: accountId,
-                        data_pagamento: formatDate(now, 1),
-                        data_transacao: formatDate(now),
-                        descricao: "des",
-                        envolvido: "inter",
-                        status: true,
-                        tipo: "REC",
-                        valor: "123"
-                    },
-                })
-            }).as('response')
-
-        cy.get('@response').its('status').should('be.equal', 201)
-        cy.get('@response').its('body.id').should('exist')
+        cy.get(loc.MENU.MOVIMENTATION).click()
+        cy.get(loc.MOVIMENTATION.DESCRIPTION).clear().type('Description')
+        cy.get(loc.MOVIMENTATION.VALUE).clear().type('250')
+        cy.get(loc.MOVIMENTATION.INTERESTED).clear().type('Interested')
+        cy.get(loc.MOVIMENTATION.ACCOUNT).select('Conta para movimentacoes')
+        cy.get(loc.MOVIMENTATION.STATUS).click()
+        cy.get(loc.MOVIMENTATION.BTN_SALVE).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+        cy.get(loc.FINANCIAL_STATEMENT.LINES).should('have.length', 7)
+        cy.xpath(loc.FINANCIAL_STATEMENT.FN_XP_SEARCH_ELEMENT('Description', '250')).should('exist')
     })
 
     it('Should get balance', () => {
-        let accountBalance = null;
-        cy.request({
-            method: 'GET',
-            url: '/saldo',
-        }).then(res => {
-            res.body.forEach(account => {
-                if (account.conta === "Conta para saldo") {
-                    accountBalance = account;
-                }
-            });
-            expect(accountBalance.saldo).to.be.equal('534.00')
-        })
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.BALANCE.FN_XP_BALANCE_ACCOUNT('Conta para saldo')).should('contain', '534,00')
 
-        cy.request({
-            method: 'GET',
-            url: '/transacoes',
-            qs: { descricao: 'Movimentacao 1, calculo saldo' }
-        }).then(res => {
-            cy.request({
-                method: 'PUT',
-                url: `/transacoes/${res.body[0].id}`,
-                body: {
-                    status: true,
-                    data_transacao: convertToDDMMYYYY(res.body[0].data_transacao),
-                    data_pagamento: convertToDDMMYYYY(res.body[0].data_pagamento),
-                    descricao: res.body[0].descricao,
-                    envolvido: res.body[0].envolvido,
-                    valor: res.body[0].valor,
-                    conta_id: res.body[0].conta_id
-                },
-            }).its('status').should('be.equal', 200)
-        })
+        cy.get(loc.MENU.FINANCIAL_STATEMENT).click()
+        cy.xpath(loc.FINANCIAL_STATEMENT.FN_XP_UPDATE_TRANSACTION('Movimentacao 1, calculo saldo')).click()
+        cy.get(loc.MOVIMENTATION.DESCRIPTION).should('have.value','Movimentacao 1, calculo saldo')
+        cy.get(loc.MOVIMENTATION.STATUS).click()
+        cy.get(loc.MOVIMENTATION.BTN_SALVE).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
 
-        cy.request({
-            method: 'GET',
-            url: '/saldo',
-        }).then(res => {
-            res.body.forEach(account => {
-                if (account.conta === "Conta para saldo") {
-                    accountBalance = account;
-                }
-            });
-            expect(accountBalance.saldo).to.be.equal('4034.00')
-        })
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.BALANCE.FN_XP_BALANCE_ACCOUNT('Conta para saldo')).should('contain', '4.034,00')
     })
-
 
     it('Should remove a transaction', () => {
-        cy.request({
-            method: 'GET',
-            url: '/transacoes',
-            qs: { descricao: 'Movimentacao para exclusao' }
-        }).then(res => {
-            cy.log(res)
-            cy.request({
-                method: 'DELETE',
-                url: `/transacoes/${res.body[0].id}`,
-            }).its('status').should('be.equal', 204)
-        })
+        cy.get(loc.MENU.FINANCIAL_STATEMENT).click()
+        cy.xpath(loc.FINANCIAL_STATEMENT.FN_XP_REMOVE_TRANSACTION('Movimentacao para exclusao')).click()
+        cy.get(loc.FINANCIAL_STATEMENT.LINES).should('have.length', 6)
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
     })
-
-    function formatDate(date, days = 0) {
-        let newDate = new Date(date);
-        newDate.setDate(newDate.getDate() + days);
-
-        let day = newDate.getDate();
-        let month = newDate.getMonth() + 1;
-        let year = newDate.getFullYear();
-
-        if (day < 10) {
-            day = '0' + day;
-        }
-        if (month < 10) {
-            month = '0' + month;
-        }
-        return `${day}/${month}/${year}`;
-    }
-
-    function convertToDDMMYYYY(dateString) {
-        const date = new Date(dateString);
-
-        const day = String(date.getUTCDate()).padStart(2, '0');
-        const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-        const year = date.getUTCFullYear();
-
-        return `${day}/${month}/${year}`;
-    }
 })
