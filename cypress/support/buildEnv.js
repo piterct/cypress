7e81236b14f70bc2f8fde97ffc3d3d1cc820956c
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-06-23 11:49:39 -0300

    buidEnv with some methods

--- cypress/support/buildEnv.js
+++ cypress/support/buildEnv.js
@@ -1,5 +1,35 @@
 const buildEnv = () => {
+    cy.intercept({
+        method: 'POST',
+        url: '/signin',
+    },
+        {
+            id: 50314,
+            nome: "False User",
+            token: "A big string"
+        }
+    ).as('signin')
 
+
+    cy.intercept({
+        method: 'GET',
+        url: '/saldo',
+    },
+        [
+            { conta_id: 999, conta: "Wallet", saldo: "100.00" },
+            { conta_id: 9909, conta: "Bank", saldo: "10000000.00" },
+        ]
+    ).as('balance')
+
+    cy.intercept({
+        method: 'GET',
+        url: '/contas'
+    },
+        [
+            { id: 1, nome: "Wallet", visivel: true, usuario_id: 1 },
+            { id: 2, nome: "Bank", visivel: true, usuario_id: 1 },
+        ]
+    ).as('accounts')
 }
 
 export default buildEnv
