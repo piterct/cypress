38cfab5f8010dd216d568d972f142453352c5bc7
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-06-23 13:35:02 -0300

    get movimentation with fixture

--- cypress/integration/barriga/frontend.spec.js
+++ cypress/integration/barriga/frontend.spec.js
@@ -110,51 +110,16 @@ describe('Should test at a functional level', () => {
             }
         ).as('transations')
 
-        cy.intercept({
-            method: 'GET',
-            url: '/extrato/**'
-        },
-            {
-                statusCode: 200,
-                body: [
-                    {
-                        conta: "Conta para movimentacoes", id: 2031036, descricao: "Movimentacao para exclusao", envolvido: "AAA", observacao: null, tipo: "DESP",
-                        data_transacao: "2024-06-23T03:00:00.000Z", data_pagamento: "2024-06-23T03:00:00.000Z", valor: "-1500.00", status: true, conta_id: 2165573,
-                        usuario_id: 50314, transferencia_id: null, parcelamento_id: null
-                    },
-                    {
-                        conta: "Conta com movimentacao", id: 2031037, descricao: "Movimentacao de conta", envolvido: "BBB", observacao: null, tipo: "DESP",
-                        data_transacao: "2024-06-23T03:00:00.000Z", data_pagamento: "2024-06-23T03:00:00.000Z", valor: "-1500.00", status: true,
-                        conta_id: 2165574, usuario_id: 50314, transferencia_id: null, parcelamento_id: null
-                    },
-                    {
-                        conta: "Conta para saldo", id: 2031038, descricao: "Movimentacao 1, calculo saldo", envolvido: "CCC", observacao: null, tipo: "REC",
-                        data_transacao: "2024-06-23T03:00:00.000Z", data_pagamento: "2024-06-23T03:00:00.000Z", valor: "3500.00", status: false,
-                        conta_id: 2165575, usuario_id: 50314, transferencia_id: null, parcelamento_id: null
-                    },
-                    {
-                        conta: "Conta para saldo", id: 2031039, descricao: "Movimentacao 2, calculo saldo", envolvido: "DDD", observacao: null, tipo: "DESP",
-                        data_transacao: "2024-06-23T03:00:00.000Z", data_pagamento: "2024-06-23T03:00:00.000Z", valor: "-1000.00", status: true,
-                        conta_id: 2165575, usuario_id: 50314, transferencia_id: null, parcelamento_id: null
-                    },
-                    {
-                        conta: "Conta para saldo", id: 2031040, descricao: "Movimentacao 3, calculo saldo", envolvido: "EEE", observacao: null, tipo: "REC",
-                        data_transacao: "2024-06-23T03:00:00.000Z", data_pagamento: "2024-06-23T03:00:00.000Z", valor: "1534.00", status: true,
-                        conta_id: 2165575, usuario_id: 50314, transferencia_id: null, parcelamento_id: null
-                    },
-                    {
-                        conta: "Conta para extrato", id: 2031041, descricao: "Movimentacao para extrato", envolvido: "FFF", observacao: null, tipo: "DESP",
-                        data_transacao: "2024-06-23T03:00:00.000Z", data_pagamento: "2024-06-23T03:00:00.000Z", valor: "-220.00", status: true,
-                        conta_id: 2165576, usuario_id: 50314, transferencia_id: null, parcelamento_id: null
-                    },
-                    {
-                        conta: "Conta para saldo", id: 2031049, descricao: "Description", envolvido: "FFF", observacao: null, tipo: "DESP",
-                        data_transacao: "2024-06-23T03:00:00.000Z", data_pagamento: "2024-06-23T03:00:00.000Z", valor: "250.00", status: true,
-                        conta_id: 2165576, usuario_id: 50314, transferencia_id: null, parcelamento_id: null
-                    }
-                ]
-            }).as('bankStatement')
-
+        cy.fixture('savedMovimentation').then(savedMovimentation => {
+            cy.intercept({
+                method: 'GET',
+                url: '/extrato/**'
+            },
+                {
+                    statusCode: 200,
+                    body: savedMovimentation
+                }).as('bankStatement')
+        })
         cy.get(loc.MENU.MOVIMENTATION).click()
         cy.get(loc.MOVIMENTATION.DESCRIPTION).clear().type('Description')
         cy.get(loc.MOVIMENTATION.VALUE).clear().type('250')
@@ -162,7 +127,6 @@ describe('Should test at a functional level', () => {
         cy.get(loc.MOVIMENTATION.ACCOUNT).select('Bank')
         cy.get(loc.MOVIMENTATION.STATUS).click()
         cy.get(loc.MOVIMENTATION.BTN_SALVE).click()
-
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
         cy.get(loc.FINANCIAL_STATEMENT.LINES).should('have.length', 7)
         cy.xpath(loc.FINANCIAL_STATEMENT.FN_XP_SEARCH_ELEMENT('Description', '250')).should('exist')
